8181acad4a636e9fe4e96057ee8d64df43f8c34a
Yunseok Kim <mywizz@gmail.com>
2011-11-17 02:16:29 +0900

    레이아웃 수정

--- application/views/layouts/front.php
+++ application/views/layouts/front.php
@@ -1,50 +1,41 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="ko">
 <head>
 	<meta charset="utf-8">
-	<meta name="description" content="">
-
 	<title><?=Title::get()?></title>
 
 	<link href="/favicon.png" rel="shortcut icon" type="image/png">
 	
 	<?=Asset::styles()?>
 	<?=Asset::scripts()?>
-	
-	
-	<!--[if (gte IE 6)&(lte IE 8)]>
-	<script src="/javascripts/html5.js"></script>
-	<script src="/javascripts/selectivizr.js"></script>
-	<noscript><link rel="stylesheet" href="" /></noscript>
-	<![endif]-->
 
-	<?=Social_util::init()?>
-	
 </head>
 
 <body>
 	<div id="fb-root"></div>
 	<header>
 		<figure></figure>
-		<h1>miniwini</h1>
 		<nav>
+			<ul>
+				
+				<? if (Authly::signed()): ?>
 			
-			<? if (Authly::signed()): ?>
+				<li>welcome, <a href="<?=URL::to('dashboard')?>"><?=Authly::get_name()?></a></li>
+				<ii><a href="<?=URL::to('auth/logout')?>">logout</a></li>
+				
+				<? else: ?>
+				
+				<li><a href="<?=URL::to('auth/login')?>">로그인</a></li>
+				<li><a href="<?=URL::to('auth/register')?>">회원 가입</a></li>
 			
-			<? else: ?>
+				<? endif; ?>
 			
-			<? endif; ?>
+			</ul>
 			
 		</nav>
 	</header>
 
 	<div id="content">
-			
-			
-
-
-
-
 		<!--...................................................
 			start of content
 		....................................................-->
@@ -54,16 +45,9 @@
 		<!--...................................................
 			end of content
 		....................................................-->
-
 	</div>
 
-
-	</div>
 
 	<footer></footer>
-	
-	
-	
-
 </body>
 </html>
