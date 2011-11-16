0ece6fecc21baa87190a9a633cb7d274b7247f5a
Yunseok Kim <mywizz@gmail.com>
2011-11-17 02:15:26 +0900

    error 페이지 정리

--- application/views/error/404.php
+++ application/views/error/404.php
@@ -1,1 +1,11 @@
-404
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+	<meta charset="utf-8">
+	<title>404 Not Found</title>
+</head>
+
+<body>
+	<h1>404 Not Found</h1>
+</body>
+</html>
--- application/views/error/500.php
+++ application/views/error/500.php
@@ -1,1 +1,18 @@
-500
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+	<meta charset="utf-8">
+	<title>500 Error</title>
+</head>
+
+<body>
+	<h1>Error</h1>
+	
+	<? if (isset($message)): ?>
+	
+	<?=$message?>
+	
+	<? endif; ?>
+	
+</body>
+</html>
