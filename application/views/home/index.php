ec4239e3c308c7804cc8d4f3abf47e00ac3b79a5
Yunseok Kim <mywizz@gmail.com>
2011-11-21 15:33:56 +0900

    첫화면 수정

--- application/views/home/index.php
+++ application/views/home/index.php
@@ -1,3 +1,11 @@
-welcome to miniwini
-
-<br><?=$_SERVER['LARAVEL_ENV']?>
+			
+			<div data-group="notification">
+				미니위니 시즌2 개발 중입니다. 환영합니다 :)
+			</div>
+			
+			
+			<div data-group="notification">
+				<a href="<?=URL::to('roadmap')?>">로드맵</a>
+			</div>
+			
+			
