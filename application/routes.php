13619993c11d259a6b11436240edec0a6771ffbf
Yunseok Kim <mywizz@gmail.com>
2011-11-21 13:28:22 +0900

    사용자 아이디를 매칭하도록 기본 라우터 수정

--- application/routes.php
+++ application/routes.php
@@ -2,8 +2,28 @@
 
 return array(
 
-	'GET /' => array('name' => 'home', 'do' => function(){
+	'GET /' => array('name' => 'home', function(){
 		return View::of_front()->partial('content', 'home/index');
 	}),
+	
+	
+	'GET /(:any)' => array('name' => 'user', function($segment){
 
+		if (in_array($segment, array('roadmap', 'commently', 'board', 'admin', 'dashboard', 'auth', 'home')))
+		{
+			return Redirect::to($segment);
+		}
+		
+		if (is_null($user = User::where_userid($segment)->first())) return Response::error(404);
+		
+		return View::of_front()->partial('content', 'user/index', array(
+			'user' => $user
+		));
+	}),
+	
+	'GET /roadmap' => function()
+	{
+		return View::of_front()->partial('content', 'home/roadmap');
+	},
+	
 );
