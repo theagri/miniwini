cd3a42b41b62538c3b8552fe80d67439c49ddf1b
Yunseok Kim <mywizz@gmail.com>
2011-11-21 13:27:30 +0900

    로드맵 페이지 추가

---
+++ application/views/home/roadmap.php
@@ -0,0 +1,15 @@
+
+				<script src="https://api.interstateapp.com/api.js?key=57d214036cc0dd7cd35f8362d9db8db1f53f66ed&amp;v=1" type="text/javascript"></script>
+				<script type="text/javascript">
+
+				  (function() {
+
+				    interstate.init( function( API ) {
+
+				      API.helpers.roadmap( '4ec551558927f64a1e001829', {"theme":"minimal"} );
+
+				    });
+
+				  })();
+
+				</script>
