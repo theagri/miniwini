0ca609c980bfa481968792b332af0e6432cdd8e1
Yunseok Kim <mywizz@gmail.com>
2011-11-21 13:31:59 +0900

    commently 설정 파일 추가

---
+++ application/config/development/commently.php
@@ -0,0 +1,54 @@
+<?php
+return array(
+	'url' => 'http://miniwini.dev/commently',
+	
+	'accounts' => function()
+	{
+		return array(
+			'default' => array(
+				'id' => Authly::get_id(),
+				'avatar_url' => Authly::get_avatar_url()
+			)
+		);
+	},
+	
+	'account_by_provider' => function($provider){
+		switch ($provider)
+		{
+			case 'default':
+				return array(
+					'author_id' => Authly::get_id(),
+					'author_name' => Authly::get_name(),
+					'author_url' => Config::get('application.url') . '/' . Authly::get_userid(),
+					'author_avatar_url' => Authly::get_avatar_url()
+				);
+		}
+		
+		return NULL;
+	},
+	
+	'after_hook' => function($page, $comment){
+		$url = $page->url;
+		preg_match('/(?:talk|share|qna)\/(\d+)$/', $url, $m);
+		if ($m)
+		{
+			// count
+			$id = $m[1];
+			$post = Post::find($id);
+			$post->comments_count += 1;
+
+			// last_commenter
+			if ($comment and $comment->author_id)
+			{
+				$post->last_commenter_id = $comment->author_id;
+				$post->last_commented_at = $comment->created_at;
+			}
+			
+			$post->save();			
+		}
+	},
+	
+	'misc' => array(
+		'append_html' => "\t\t\t\t\t"
+	)
+);
