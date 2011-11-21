c696d3368d20543085c1bb49beddd451af8cea5f
Yunseok Kim <mywizz@gmail.com>
2011-11-21 15:31:02 +0900

    대쉬보드 추가

--- application/views/dashboard/index.php
+++ application/views/dashboard/index.php
@@ -1,24 +1,13 @@
 
 
-<? foreach (Authly::connections() as $connection): ?>
-
-<div>
-	<?=$connection->provider?> / <?=$connection->auth_id?> / <?=$connection->auth_userid?> / <?=$connection->auth_name?>
-</div>
-
-<? endforeach; ?>
+			<?=HTML::link('auth/edit', '정보 수정')?>
 
 
-<hr>
+			<? if (Config::get('authly.connections.enabled')): ?>
+				
+			<?=HTML::link('auth/connections', 'Connections')?>
+			
+			<? endif; ?>
 
 
-<a href="<?=URL::to('auth/connect/twitter')?>">twitter</a><br>
-<a href="<?=URL::to('auth/connect/facebook')?>">facebook</a><br>
-<a href="<?=URL::to('auth/connect/linkedin')?>">linkedin</a><br>
-<a href="<?=URL::to('auth/connect/google')?>">google</a><br>
-<a href="<?=URL::to('auth/connect/foursquare')?>">foursquare</a><br>
 
-<?=Form::open('auth/connect/openid')?>
-<?=Form::text('openid_identifier')?>
-<?=Form::submit('Submit')?>
-<?=Form::close()?>
