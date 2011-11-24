663dbf64385426f045dd08e181889493f1a3e02d
Yunseok Kim <mywizz@gmail.com>
2011-11-25 02:44:45 +0900

    대쉬보드 네이밍 변경

--- application/views/dashboard/_header.php
+++ application/views/dashboard/_header.php
@@ -4,7 +4,7 @@
 					<li data-tab=""><a href="#">알림</a></li>
 					<li data-tab=""><a href="#">메시지</a></li>
 					<li data-tab="" data-align="right"><a href="#">계정 연결</a></li>
-					<li data-tab="" data-align="right"><a href="<?=URL::to('auth/edit')?>">개인정보 수정</a></li>
+					<li data-tab="" data-align="right"><a href="<?=URL::to('auth/edit')?>">환경 설정</a></li>
 				</ul>
 				<div></div>
 			</div>
