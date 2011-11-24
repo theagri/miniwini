feaa00b1e074091c6f5e474f6e4b4981a32640c9
Yunseok Kim <mywizz@gmail.com>
2011-11-24 14:21:16 +0900

    미니위니, 어플리케이션, 세션 설정 변경

--- application/config/miniwini.php
+++ application/config/miniwini.php
@@ -1,10 +1,27 @@
 <?php
 return array(
 	'title' => '미니위니',
+	'description' => '미니위니',
 	'open' => TRUE,
 	
 	'contact' => array(
 		'name' => '미니위니',
 		'email' => 'mywizz@gmail.com'
 	),
+	
+	'avatar' => array(
+		'sizes' => array(
+			'small' => 24,
+			'medium' => 30,
+			'big' => 60,
+			'huge' => 100
+		),
+		'max_size' => 100,
+		'no_avatar_url' => Config::get('application.url') . '/img/no_avatar.png'
+	),
+	
+	'user' => array(
+		'min_userid_size' => 2,
+		'max_userid_size' => 10
+	)
 );
