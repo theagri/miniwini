e1a1936c3569576f7e43c7ebb2f63b2cfb017de2
Yunseok Kim <mywizz@gmail.com>
2011-11-24 18:57:08 +0900

    연재물 템플릿 수정

--- application/views/board/series.php
+++ application/views/board/series.php
@@ -6,3 +6,50 @@
 			<?=View::make('board/_tabs', array(
 				'board' => $board
 			))->get()?>
+			
+			<section data-group="board" data-type="post">
+				
+				<h1><?=$series->title?></h1>
+
+				<? foreach (array_reverse($series->posts) as $p): ?>
+				
+				<article data-type="post" data-mode="listing"<?=(Time::is_today($p->created_at) ? ' class="today"':'')?>>
+					<?=$p->user->avatar('medium')?>
+		
+					<h1>
+			
+						<a href="<?=$p->link($board->alias)?>"><?=$p->short_title?></a> 
+			
+						<?if ($p->comments_count > 0):?>
+			
+						<a data-type="comments-count" href="<?=$p->link($board->alias)?>"><span><?=number_format($p->comments_count)?></span></a>
+			
+						<? endif; ?>
+			
+					</h1>
+		
+					<p data-type="summary">
+		
+						<?=$p->summary?>
+		
+					</p>
+	
+					<footer>
+						<a data-type="user" href="<?=$p->user->link?>"><?=$p->user->name?></a>
+						/
+						<?=Time::humanized_html($p->created_at)?> / 조회 <strong><?=number_format($p->views_count)?></strong>
+						
+						<? if ($p->last_commenter): ?>
+		
+						/ 최근 댓글<?=$p->last_commenter->avatar('small')?>
+		
+						<? endif; ?>
+
+					</footer>
+		
+				</article>
+			
+				<? endforeach; ?>
+				
+				
+			</section>
