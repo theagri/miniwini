6e5821e85dbb06cd7922ba58a76f5e71f780596a
Yunseok Kim <mywizz@gmail.com>
2011-11-25 17:11:25 +0900

    아바타 만드는 사이트 링크

--- application/views/home/index.php
+++ application/views/home/index.php
@@ -9,6 +9,10 @@
 					구글 크롬, 파이어폭스, 오페라 등의 최신 브라우져를 이용해 주시기 바랍니다. 기타 브라우져는 마지막에 확인할 예정입니다.
 				</div>
 				
+				<div data-group="notification">
+					아바타는 <a href="http://faceyourmanga.com/" target="_blank">faceyourmanga.com</a>에서 만드시면 보기 좋습니다(?)
+				</div>
+				
 			</section>
 			
 			
