40d3df4f6d5b1b9a15ffaa8692ed4ef0bf9994a4
Yunseok Kim <mywizz@gmail.com>
2011-11-28 13:26:54 +0900

    Google Analytics 추가

--- application/views/layouts/front.php
+++ application/views/layouts/front.php
@@ -180,5 +180,17 @@
 	</footer>
 	
 	<div id="fb-root"></div>
+	
+	
+	<script type="text/javascript">
+	window._gaq = [['_setAccount','UA-297919-5'],['_trackPageview'],['_trackPageLoadTime']];
+	$(function(){
+		var ga = document.createElement('script'); ga.type = 'text/javascript'; 
+		ga.async = true;
+		ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+		var s = document.getElementsByTagName('script')[0]; 
+		s.parentNode.insertBefore(ga, s);
+	})();
+	</script>
 </body>
 </html>
