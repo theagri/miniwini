84e249f0903d88c173f34d286389fae34d76c0a9
Yunseok Kim <mywizz@gmail.com>
2011-11-28 15:05:27 +0900

    구글 통계 수정

--- application/views/layouts/front.php
+++ application/views/layouts/front.php
@@ -182,13 +182,13 @@
 	<div id="fb-root"></div>
 	
 	
-	<script type="text/javascript">
+	<script>
 	window._gaq = [['_setAccount','UA-297919-5'],['_trackPageview'],['_trackPageLoadTime']];
-	$(function(){
+	(function(){
 		var ga = document.createElement('script'); ga.type = 'text/javascript'; 
 		ga.async = true;
 		ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-		var s = document.getElementsByTagName('script')[0]; 
+		var s = document.getElementsByTagName('script')[0];
 		s.parentNode.insertBefore(ga, s);
 	})();
 	</script>
