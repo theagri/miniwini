868d30e32d9af2593a32d5551becb9be92b40dbb
Yunseok Kim <mywizz@gmail.com>
2011-12-02 00:06:07 +0900

    markdown으로 작성된 게시물의 summary 뽑는 로직 변경

--- application/models/post.php
+++ application/models/post.php
@@ -100,8 +100,8 @@ class Post extends Blaze {
 		{
 			case 'markdown':
 				$markdown = new Markdown();
-				$summary = $markdown->parse($this->body);
-				return e(strip_tags(mb_substr($summary, 0, 140, 'UTF-8')));
+				$summary = strip_tags($markdown->parse($this->body));
+				return e(mb_substr($summary, 0, 140, 'UTF-8'));
 				
 			default:
 				return e(strip_tags(mb_substr($this->body, 0, 140, 'UTF-8')));
