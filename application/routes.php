a354cf741d4e2e32478d198969f8b7a9d9daec66
Yunseok Kim <mywizz@gmail.com>
2011-12-05 03:01:29 +0900

    홈 라우터 수정

--- application/routes.php
+++ application/routes.php
@@ -4,22 +4,29 @@ return array(
 	// ---------------------------------------------------------------------
 
 	'GET /' => array('name' => 'home', function(){
-
-		foreach (array('talk', 'share', 'qna') as $alias)
+		
+		$boards = array(
+			'talk' => '자유게시판', 
+			'share' => '알짜게시판',
+			'qna' => '질문&amp;답변'
+		);
+		
+		foreach ($boards as $alias => $title)
 		{
 			$board = Board::aliased($alias);
 			$posts[$alias] = $board->posts()->with('user')->where_state('open')->order_by('id', 'desc')->take(10)->get();
 		}
 		
 		return View::of_front()->nest('content', 'home/index', array(
-			'posts' => $posts
+			'posts' => $posts,
+			'boards' => $boards
 		));
 	}),
 	
 	// ---------------------------------------------------------------------
 	
 	'GET /(:any)' => array('name' => 'user', function($segment){
-		if (in_array($segment, array('mobile', 'roadmap', 'commently', 'ajax', 'board', 'admin', 'dashboard', 'auth', 'home')))
+		if (in_array($segment, array('m', 'roadmap', 'commently', 'ajax', 'board', 'admin', 'dashboard', 'auth', 'home')))
 		{
 			if (strpos('/', $segment) === FALSE)
 			{
@@ -43,7 +50,7 @@ return array(
 	
 	// ---------------------------------------------------------------------
 	
-	'GET /mobile' => function(){
+	'GET /m' => function(){
 		return Response::error(404);
 		$mobile = Session::get('mobile');
 		Session::put('mobile', ! $mobile);
