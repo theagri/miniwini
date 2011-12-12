21c52c1cb93ca508ca747d8d618424a94a204d72
Yunseok Kim <mywizz@gmail.com>
2011-12-12 11:49:00 +0900

    라우터 수정

--- application/routes/ajax.php
+++ application/routes/ajax.php
@@ -9,6 +9,23 @@ return array(
 		}
 		
 		return Post::preview($body);
+	},
+	
+	'GET /ajax/find_user' => function(){
+		$k = trim(Input::get('keyword'));
+		if ( ! $k)
+		{
+			return '';
+		}
 		
+		$k = '%'. $k . '%';
+		$users = DB::table('users')->select(array('id', 'userid', 'name', 'avatar_url'))->where('userid', 'LIKE', $k)->or_where('name', 'LIKE', $k)->get();
+		die(json_encode($users));
+	},
+	
+	'GET /ajax/commently/(:num)' => function($id)
+	{
+		$comment = Commently::comment($id);
+		die(json_encode($comment));
 	}
 );
